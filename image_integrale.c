e89b1952e5a99bfc69b4ddb5345ec14149f40649
Rubixbob <julienhans@hotmail.fr>
2015-05-20 10:28:46 +0200

    Ajoute pixmap

---
+++ image_integrale.c
@@ -0,0 +1,22 @@
+// x >
+// y V
+
+image integral_image(image i) {
+	image ii;
+
+	// Ligne 0
+	ii(0,0) = i(0,0);	// Première case
+	for (i = 1; i < XMAX; i++) {
+		ii(i,0) = i(i,0) + ii(i-1, 0);
+	}
+
+	// Lignes restantes
+	for (j = 1; j < YMAX; j++) {
+		ii(0,j) = i(0,j) + ii(0, j-1);	// Première colonne
+		for (i = 1; i < XMAX; i++) {
+			ii(i,j) = i(i,j) + ii(i, j-1) + ii(i-1, j) - ii(i-1, j-1);
+		}
+	}
+
+	return ii;
+}
