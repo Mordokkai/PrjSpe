3f9ff885d8168d09da0b860ce7afe2c29ce67efd
Saad Ouassil Allak <sw.allak@gmail.com>
2015-05-22 13:13:18 +0200

    Reglages Problèmes git

--- image_integrale/image_io.h
+++
@@ -1,6 +0,0 @@
-#ifndef IMAGE_IO__H
-#define IMAGE_IO__H
-
-unsigned char* lire_image(char* fichier, int* width, int* height, int* lumin);
-void ecrire_image(char* fichier, unsigned char* image, int width, int height, int lumin);
-#endif
