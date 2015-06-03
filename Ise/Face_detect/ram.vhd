5ae5b69ea4a756c13743bc7dbc1c7c4df3488f7f
Sylvain Guy <S.Guy09@laposte.net>
2015-06-03 11:20:43 +0200

    corrections

--- Ise/Face_detect/ram.vhd
+++ Ise/Face_detect/ram.vhd
@@ -1,8 +1,24 @@
+--library IEEE;
+--        use IEEE.std_logic_1164.all;
+--        use IEEE.std_logic_textio.all;
+--        use IEEE.std_logic_arith.all;
+--        use IEEE.numeric_bit.all;
+--        use IEEE.numeric_std.all;
+--        use IEEE.std_logic_signed.all;
+--        use IEEE.std_logic_unsigned.all;
+--        use IEEE.math_real.all;
+--        use IEEE.math_complex.all;
+--
+--        library STD;
+--        use STD.textio;
+
+
 --Déclaration RAM_image
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 USE ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 
 entity ram_image is
@@ -42,17 +58,18 @@ end Behavioral;
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 USE ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 
 
 --Déclaration RAM_II
 
 entity ram_II is
-port (Clk : in std_logic;
+port ( clk : in std_logic;
         address : in integer;
         we : in std_logic;
-        data_i : in std_logic_vector(31 downto 0);
-        data_o : out std_logic_vector(31 downto 0)
+        data_i : in unsigned(31 downto 0);
+        data_o : out unsigned(31 downto 0)
      );
 end ram_II;
 
@@ -60,15 +77,15 @@ architecture Behavioral of ram_II is
 
 --Declaration of type and signal of a 256 element RAM
 --with each element being 8 bit wide.
-type ram_t is array (0 to 128*64) of std_logic_vector(31 downto 0);
+type ram_t is array (0 to 128*64) of unsigned(31 downto 0);
 signal ram : ram_t := (others => (others => '0'));
 
 begin
 
 --process for read and write operation.
-PROCESS(Clk)
+PROCESS(clk)
 BEGIN
-    if(rising_edge(Clk)) then
+    if(rising_edge(clk)) then
         if(we='1') then
             ram(address) <= data_i;
         end if;
