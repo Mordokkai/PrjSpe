d89e8bae0882c15035b85057082ba13563fa82bb
Saad Ouassil Allak <sw.allak@gmail.com>
2015-06-04 13:52:38 +0200

    function to calculate the minimum bits needed to convert floating poit to fixed point

--- c/detection_vigule_fixe/main.c
+++ c/detection_vigule_fixe/main.c
@@ -13,28 +13,37 @@
                                 }\
                             printf("\n");\
                           }
+void convertCascade(Cascade cascade, char* outname, int se, int sv,int fe, int fv)
+{
+    StageList s=cascadeToStageList(&cascade);
+    StageList p;
+    for (p=s; p!=NULL; p=p->next)
+    {
 
+    }
+}
 
-int main()
+int main(int argc, char ** argv)
 {
     printf("Hello world!\n");
-    double a,b,c,d;
-//    Cascade* cascade =openCascade("/home/swallak/Documents/SLE 2A/Projet_Spé/PrjSpe/c/detection_vigule_fixe/cascade/haarcascade_frontalface_default.txt");
-//
+//    double a,b,c,d;
+//    Cascade* cascade =openCascade(argv[1]);
 //    maxThresholds(*cascade,&a,&b,&c,&d);
-    //printf("%lf %lf %lf %lf\n",a,b,c,d);
-
-    FixedPoint fx1, fx2;
-    bool init1 = initFixedPoint(&fx1, 5,8);
-    bool init2 = initFixedPoint(&fx2,3,6);
-    if (!init1 || !init2)
-        printf("Not sufficient!!!\n");
-
-    convertDouble(-4 ,&fx1);
-    convertDouble(-4, &fx2);
+//    printf("%lf %lf %lf %lf\n",a,b,c,d);
+//
+//    FixedPoint fx1, fx2;
+//    bool init1 = initFixedPoint(&fx1, 5,8);
+//    bool init2 = initFixedPoint(&fx2,3,6);
+//    if (!init1 || !init2)
+//        printf("Not sufficient!!!\n");
+//
+//    convertDouble(-4 ,&fx1);
+//    convertDouble(-4, &fx2);
+    minE(8.);
+    //printf("%d");
     //int* fx2tab = fixedPointtoTab(fx2);
     //PRINTTAB2(fx2tab, 10,3);
 
-    FixedPoint result = PLUS(fx1, fx2);
+//    FixedPoint result = PLUS(fx1, fx2);
     return 0;
 }
