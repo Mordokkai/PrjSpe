f8d7be4cd9f1fe1258858d8f238a18dbd8bd2abc
Saad Ouassil Allak <sw.allak@gmail.com>
2015-06-04 15:59:02 +0200

    generation cascade avec seuils en virgule fixe

--- c/detection_vigule_fixe/main.c
+++ c/detection_vigule_fixe/main.c
@@ -13,24 +13,68 @@
                                 }\
                             printf("\n");\
                           }
-void convertCascade(Cascade cascade, char* outname, int se, int sv,int fe, int fv)
+void convertCascade(Cascade cascade, char* outname, int se, int sv,int fe, int fv, int gte, int gtv, int lse, int lsv)
 {
+    FILE* f = fopen(outname, "w+");
     StageList s=cascadeToStageList(&cascade);
     StageList p;
     for (p=s; p!=NULL; p=p->next)
     {
+        char c;
+        for(int i=0; i<p->element.numberOfFeatures; i++)
+        {
+            c ='R';
+            for(int j=0; j<p->element.features[i].type; j++)
+            {
+                int x,y,w,h,weight;
+                x=p->element.features[i].rect[j].x;
+                y=p->element.features[i].rect[j].y;
+                w=p->element.features[i].rect[j].weight;
+                h=p->element.features[i].rect[j].h;
+                weight=(int)p->element.features[i].rect[j].weight;
+                fprintf(f,"%c %d %d %d %d %d\n",c, x, y, w, h, weight);
 
+
+            }
+            c='F';
+            FixedPoint fth, gt, ls;
+            initFixedPoint(&fth, fe,fv);
+            initFixedPoint(&gt, gte, gtv);
+            initFixedPoint(&ls, lse,lsv);
+            convertDouble(p->element.features[i].f,&fth);
+            convertDouble(p->element.features[i].gt,&gt);
+            convertDouble(p->element.features[i].ls,&ls);
+            fprintf(f, "%c %d %d %d\n",c, fth.val, gt.val, ls.val);
+        }
+        c='S';
+        FixedPoint sth;
+        initFixedPoint(&sth, se, sv);
+        convertDouble(p->element.threshold,&sth);
+        fprintf(f, "%c %d", c, sth.val);
     }
 }
 
 int main(int argc, char ** argv)
 {
     printf("Hello world!\n");
-//    double a,b,c,d;
-//    Cascade* cascade =openCascade(argv[1]);
-//    maxThresholds(*cascade,&a,&b,&c,&d);
-//    printf("%lf %lf %lf %lf\n",a,b,c,d);
-//
+    double sth,fth,gt,ls;
+    Cascade* cascade =openCascade(argv[1]);
+    maxThresholds(*cascade,&sth,&fth,&gt,&ls);
+    printf("%lf %lf %lf %lf\n",sth,fth,gt,ls);
+
+    int se = minE(sth);
+    int sv = SIZE-se-1;
+    int fe = minE(fth);
+    int fv = SIZE-fe-1;
+    int gte = minE(gt);
+    int gtv = SIZE-gte-1;
+    int lse = minE(ls);
+    int lsv = SIZE-lse-1;
+
+    printf("\n%d %d %d %d\n", se, fe, gte, lse);
+    convertCascade(*cascade,"test.txt",se,sv,fe,fv,gte,gtv,lse,lsv);
+
+
 //    FixedPoint fx1, fx2;
 //    bool init1 = initFixedPoint(&fx1, 5,8);
 //    bool init2 = initFixedPoint(&fx2,3,6);
