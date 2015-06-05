cf3d833e47d99e97eb600600353ac01ebc752f01
Sylvain Guy <S.Guy09@laposte.net>
2015-06-05 22:00:32 +0200

    fsm

--- Ise/Face_detect/fsm_detection.vhd
+++ Ise/Face_detect/fsm_detection.vhd
@@ -17,8 +17,15 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+--USE ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+ library work;
+ use work.Face_DETECTION_PCK.all;
+--use ieee.std_logic_arith.all;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,22 +37,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity fsm_detection is
-			Port ( Image_int_ready : in  STD_LOGIC;
-          clk : in  STD_LOGIC;
-          rst : in  STD_LOGIC;
+			Port (Image_int_ready : in  boolean;
+					clk : in  STD_LOGIC;
+					rst : in  STD_LOGIC;
 					
 					--Communications avec les RAMS
-					we_stage : out std_logic
-					ad_stage : out integer;
-					datao_stage : in unsigned(31 downto 0);
+					we_stage : out std_logic;
+					ad_stage : out integer;--fjydfjydjydtyk
+					datao_stage : in unsigned(27 downto 0);
 					
 					we_feature : out std_logic;
-					ad_feature : out std_logic;
-					datao_feature : in unsigned(59 downto 0);
+					ad_feature : out integer;
+					datao_feature : in unsigned(60 downto 0);
 
 					we_rectangle : out std_logic;
-					ad_rectangle : out std_logic;
-					datao_rectangle : in unsigned(31 downto 0);
+					ad_rectangle : out integer;
+					datao_rectangle : in unsigned(24 downto 0);
 
 					we_II	: out std_logic;
 					ad_II:	out integer;
@@ -55,42 +62,65 @@ entity fsm_detection is
 					we_II_2 : out std_logic;
 					datao_II_2: in unsigned(39 downto 0);
 
-          face_detected : out  STD_LOGIC;
-          send_img : out  STD_LOGIC);
+          face_detected : out  boolean;
+          send_img : out  boolean);
 end fsm_detection;
 
 architecture Behavioral of fsm_detection is
 
-type   STATE is (Wait_RDY, Place_Detector, Req_s, Req_meanvar, D_meanvar, Req_f, Req_r, D_r, RAM_II, Eval_f, Eval_s);
+type   STATE is (Wait_RDY, Place_Detector, Req_s, Req_meanvar, D_meanvar, Req_f, Req_r, D_r, RAM_II, D_f, Eval_f, Eval_s);
     	signal Current_State, Next_State   : STATE;
-    	signal access_count, access_count_next : std_logic_vector(3 downto 0);
+    	signal access_count, access_count_next : integer;
     	signal if_q, if_d : integer;
     	signal is_q, is_d : integer;
-			signal ir_q, ir_d : integer;
-			signal ik_q, ik_d : integer;
-			signal l_q, l_d : integer;
-			signal m_q, m_d : integer);
-			signal mean_q, mean_d: Mean;
-			signal var_q, var_d: Var;
-			signal r_q, r_d: R;
-			signal rect_q,rect_d: Rectangles;
-			signal sums_q,sums_d: integer;
-			signal sumf_q,sumf_d: integer;
-			signal img_Termine_q, img_Termine_d : boolean;
-			signal det_q,det_d : Detector;
-			signal s_q, s_d: Stage;
-			signal f_q, f_d: Feature;
+		signal ir_q, ir_d : integer;
+		signal ik_q, ik_d : integer;
+		signal l_q, l_d : integer;
+		signal m_q, m_d : integer;
+		signal mean_q, mean_d: Mean;
+		signal var_q, var_d: Var;
+		signal r_q, r_d: R_tab;
+		signal rect_q,rect_d: Rectangles;
+		signal sums_q,sums_d: integer;
+		signal sumf_q,sumf_d: integer;
+		signal img_Termine_q, img_Termine_d : boolean;
+		signal det_q,det_d : Detector;
+		signal s_q, s_d: Stage;
+		signal f_q, f_d: Feature;
+		
+		
+		we_stage <= '0';
+		ad_stage <= 0;
+		--datao_stage <= (others => 0);
+					
+		we_feature <= '0';
+		ad_feature <= 0;
+		--datao_feature : in unsigned(60 downto 0);
+
+		we_rectangle <= '0';
+		ad_rectangle <= 0;
+		--datao_rectangle : in unsigned(24 downto 0);
+
+		we_II	:<= '0';
+		ad_II <= 0;
+		--datao_II: in unsigned(31 downto 0);
+					
+		ad_II_2 <= 0;
+		we_II_2 <= '0';
+		--datao_II_2: in unsigned(39 downto 0);
+
+          
 
 begin
 
 	P_STATE : process(clk)
 	begin
 		if rising_edge(clk) then
-			if (RESET = '1') then
+			if (Rst = '1') then
 				current_state <= Wait_RDY;
        
 			else
-        access_count <= access_count_next;
+				access_count <= access_count_next;
 				if_q <= if_d;
 				is_q <= is_d;
 				ir_q <= ir_d;
@@ -104,7 +134,7 @@ begin
 				sums_q <= sums_d;
 				sumf_q <= sumf_d;
 				img_Termine_q <= img_Termine_d;
-      	Current_State <= Next_State;
+				Current_State <= Next_State;
 				det_q <= det_d;
 				s_q <= s_d;
 				f_q <= f_d;
@@ -118,6 +148,7 @@ begin
 	P_FSM : process(clk)
 	variable calcul : integer;
 	variable Stage_OK : boolean;
+	variable passe_Feature : boolean;
    begin
      	case current_state is 		
 					
@@ -126,7 +157,7 @@ begin
 					next_state <= Place_Detector;
 				else 
 					--On positionne le détecteur à sa position initiale
-					det_d.x <= 24;
+					det_d.x <= -1;
 					det_d.y <= 0;
 					next_state <= Wait_RDY;
 				end if;
@@ -135,14 +166,14 @@ begin
 
 			when Place_Detector =>
 			--Deplacement du détecteur
-				if(det_q.y > IMG_WIDTH-24) then
-					det_d.x <= det_q.x+1;
-					det_d.y <= 0;
-				else 
+				if(det_q.x > IMG_WIDTH-24) then
 					det_d.y <= det_q.y+1;
+					det_d.x <= 0;
+				else 
+					det_d.x <= det_q.x+1;
 				end if; 
 			--Changement d'état
-				if(det_q.x=IMG_HEIGHT and det_q.y=IMG_WIDTH-24) then
+				if(det_q.y=IMG_HEIGHT-25 and det_q.x=IMG_WIDTH-25) then
 					img_Termine_d <= true;
 				else
 					img_Termine_d <= false;
@@ -156,10 +187,10 @@ begin
 			--Récupération des valeurs
 				we_stage <= '0';
 				ad_stage <= is_d;
-				we_II <= 0;
-				ad_II <= (det_q.x)*IMG_WIDTH+det_q.y; -- Le coin en bas à gauche du détecteur
-				we_II_2 <= 0;
-				ad_II_2 <= (det_q.x)*IMG_WIDTH+det_q.y; --idem
+				we_II <= '0';
+				ad_II <= (det_q.y)*IMG_WIDTH+det_q.x; -- Le coin en bas à gauche du détecteur
+				we_II_2 <= '0';
+				ad_II_2 <= (det_q.y)*IMG_WIDTH+det_q.x; --idem
 					
 				--Changement d'état
 				ik_d <= 0;
@@ -169,14 +200,14 @@ begin
 			when Req_meanvar =>
 			--Récupération des valeurs
 				--s <= datao_stage;--A détailler
-				s.threshold <= datao_stage(31 downto 12);
-				s.ad_feature <= datao_stage(11 downto 0);
-				mean_d(ik_q) <= datao_II;
-				var_d(ik_q) <= datao_II_2;
-				we_II <= 0;
-				ad_II <= (det_q.x - 24* m_q)*IMG_WIDTH+det_q.y + 24*l_q;
-				we_II_2 <= 0;
-				ad_II_2 <= (det_q.x - 24*m_q)*IMG_WIDTH+det_q.y + 24*l_q;
+				s_d.threshold <= to_integer(datao_stage(27 downto 12));
+				s_d.ad_feature <= to_integer(datao_stage(11 downto 0));
+				mean_d(ik_q) <= to_integer(datao_II);
+				var_d(ik_q) <= to_integer(datao_II_2);
+				we_II <= '0';
+				ad_II <= (det_q.y + 24* m_q)*IMG_WIDTH+det_q.x + 24*l_q;
+				we_II_2 <= '0';
+				ad_II_2 <= (det_q.y + 24*m_q)*IMG_WIDTH+det_q.x + 24*l_q;
 						
 			--Changement d'état
 				if(ik_q<3) then
@@ -203,8 +234,8 @@ begin
 
 			when D_meanvar =>	--Etape de transition
 			--Récupération des valeurs
-				mean_d(ik_q) <= datao_II;
-				var_d(ik_q) <= datao_II_2;
+				mean_d(ik_q) <= to_integer(datao_II);
+				var_d(ik_q) <= to_integer(datao_II_2);
 
 			--Changement d'état
 				if_d <= 0;
@@ -213,7 +244,7 @@ begin
 
 			when Req_f =>
 			--Récupération des valeurs
-				we_feature <= 0;
+				we_feature <= '0';
 				ad_feature <= s_q.ad_feature + if_q;
 
 			--Changement d'état
@@ -226,16 +257,15 @@ begin
 
 			when Req_r =>
 			--Récupération des valeurs
-				--f <= datao_feature;--A detailler
-				f_d.threshold <= datao_feature(59 downto 44);
-				f_d.greater <= datao_feature(27 downto 12);
-				f_d.lower <= datao_feature(43 downto 28);
-				f_d.ad_rectangle <= datao_feature(11 downto 0);
+				f_d.threshold <= to_integer(datao_feature(60 downto 45));
+				f_d.greater <= to_integer(datao_feature(28 downto 13));
+				f_d.lower <= to_integer(datao_feature(44 downto 29));
+				f_d.ad_rectangle <= to_integer(datao_feature(12 downto 0));
 				next_state <= D_f;
 				
 			when D_f =>
-				we_rectangle <= 0;
-				ad_rectangle <= f.ad_rectangle + ir_q;
+				we_rectangle <= '0';
+				ad_rectangle <= f_q.ad_rectangle + ir_q;
 				
 			--Changement d'état
 				if(ir_q < f_q.nr) then
@@ -247,33 +277,27 @@ begin
 				
 
 			when D_r =>
-				--r(ir_q) <= datao_rectangle;
-				--we_II <= 0;
-				--ad_II <= r(ir_q) srl (12+5*z_q);	
-				--z_d <= z_q -1;
-				--next_state <= RAM_II;
-				rect_d(ir_q).x <= datao_rectangle(31 downto  27);
-				rect_d(ir_q).y <= datao_rectangle(26 downto  22);
-				rect_d(ir_q).h <= datao_rectangle(21 downto  17);
-				rect_d(ir_q).w <= datao_rectangle(16 downto  12);
-				rect_d(ir_q).weight <= datao_rectangle(11 downto  0);
+				rect_d(ir_q).x <= to_integer(datao_rectangle(24 downto  20));
+				rect_d(ir_q).y <= to_integer(datao_rectangle(19 downto  15));
+				rect_d(ir_q).h <= to_integer(datao_rectangle(14 downto  10));
+				rect_d(ir_q).w <= to_integer(datao_rectangle(9 downto  5));
+				rect_d(ir_q).weight <= to_integer(datao_rectangle(5 downto  0));
 				next_state <= RAM_II;
 
 			when RAM_II =>
-				--r_d(access_count_q) <= datao_II;
-				we_II <= 0;
+				we_II <= '0';
 				if(access_count = 0) then
-					ad_II <= (det_q.x - rect_q.x) * IMG_WIDTH + (det_q.y + rect_q.y) ; 
+					ad_II <= (det_q.x + rect_q(ir_q).x) * IMG_WIDTH + (det_q.y + rect_q(ir_q).y) ; 
 				elsif(access_count = 1) then
-					ad_II <= (det_q.x - rect_q.x - rect_q.h) * IMG_WIDTH + (det_q.y + rect_q.y); 
+					ad_II <= (det_q.x + rect_q(ir_q).x + rect_q(ir_q).h) * IMG_WIDTH + (det_q.y + rect_q(ir_q).y); 
 				elsif(access_count = 2) then
-					ad_II <= (det_q.x - rect_q.x) * IMG_WIDTH + (det_q.y + rect_q.y + rect_q.w); 
+					ad_II <= (det_q.x + rect_q(ir_q).x) * IMG_WIDTH + (det_q.y + rect_q(ir_q).y + rect_q(ir_q).w); 
 				elsif(access_count = 3) then
-					ad_II <= (det_q.x - rect_q.x - rect_q.h) * IMG_WIDTH + (det_q.y + rect_q.y + rect_q.w); 
+					ad_II <= (det_q.x + rect_q(ir_q).x - rect_q(ir_q).h) * IMG_WIDTH + (det_q.y + rect_q(ir_q).y + rect_q(ir_q).w); 
 				end if;
 				
-				if(access_count_q <4) then 
-					access_count_d <= access_count_q+1;
+				if(access_count <4) then 
+					access_count_next <= access_count+1;
 					next_state <= RAM_II;
 				else
 					ir_d <= ir_q +1 ;
@@ -283,11 +307,12 @@ begin
 			
 			when Eval_f => 
 				--Calcul
-				calcul := FeatureEval(r_q,rect_q);			
-				if(calcul < f_d.threshold) then
-					sumf_d <= sumf_q + f_d.greater;
+				calcul := FeatureEval(r_q,rect_q);
+				passe_Feature := test_Feature(sumf_q, mean_q, var_q, calcul, f_q);
+				if(passe_Feature) then
+					sumf_d <= sumf_q + f_q.greater;
 				else
-					sumf_d <= sumf_q + f_d.lower;
+					sumf_d <= sumf_q + f_q.lower;
 				end if;
  				--Changement d'état
 				if(if_q < s_d.nf) then
@@ -297,18 +322,19 @@ begin
 				end if;
 				
 			when Eval_s =>
-				if(sumf_d > s_d.threshold) then
+				if(sumf_d >= s_d.threshold) then
 					Stage_OK := true;
-				if(is_q < Ns and Stage_OK) then --TODO
+				end if;
+				if(is_q < Nstage and Stage_OK) then --TODO
 					next_state <= Req_s;
-				elsif (is_q=Ns) then
+				elsif (is_q=Nstage) then
 					face_detected <= true;
 					next_state <= Place_Detector;
 				elsif (not Stage_OK) then
 					face_detected <= false;
 					next_state <= Place_Detector;
 				else
-					img_Termine <= true;
+					img_Termine_d <= true;
 					next_state <= Wait_RDY;
 				end if;
 					
@@ -316,4 +342,5 @@ begin
 
 			end case;
 		end process P_FSM;
+		
 end  Behavioral;
