6eae5d1c97a17ff4cc000c6f882a2c771fb8d28d
Sylvain Guy <S.Guy09@laposte.net>
2015-06-05 21:52:49 +0200

    suite

--- Ise/Face_detect/bench_II.vhd
+++ Ise/Face_detect/bench_II.vhd
@@ -5,6 +5,8 @@ library std;
 use std.textio.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
+library work;
+use work.all;
 
 
 
@@ -34,6 +36,65 @@ COMPONENT RAM_II_2  --'test' is the name of the module needed to be tested.
          we : IN  std_logic;
 			data_i : IN  unsigned(39 downto 0);
 			data_o : out  unsigned(39 downto 0)
+        );
+    END COMPONENT;
+	 
+	 
+COMPONENT RAM_stages  --'test' is the name of the module needed to be tested.
+    PORT(
+         clk : IN  std_logic;
+         adress : in  integer;
+			data_o : out  unsigned(27 downto 0)
+        );
+    END COMPONENT;
+	 
+COMPONENT RAM_features  --'test' is the name of the module needed to be tested.
+    PORT(
+         clk : IN  std_logic;
+         adress : in  integer;
+			data_o : out  unsigned(60 downto 0)
+        );
+    END COMPONENT;
+	 
+COMPONENT RAM_rectangles  --'test' is the name of the module needed to be tested.
+    PORT(
+         clk : IN  std_logic;
+         adress : in  integer;
+			data_o : out  unsigned(24 downto 0)
+        );
+    END COMPONENT;
+	 
+	 
+	 
+COMPONENT FSM_DETECTION  --'test' is the name of the module needed to be tested.
+    PORT(
+			Image_int_ready : in  boolean;
+          clk : in  STD_LOGIC;
+          rst : in  STD_LOGIC;
+					
+			--Communications avec les RAMS
+			we_stage : out std_logic;
+			ad_stage : out integer;
+			datao_stage : in unsigned(27 downto 0);
+					
+			we_feature : out std_logic;
+			ad_feature : out integer;
+			datao_feature : in unsigned(60 downto 0);
+
+			we_rectangle : out std_logic;
+			ad_rectangle : out integer;
+			datao_rectangle : in unsigned(24 downto 0);
+
+			we_II	: out std_logic;
+			ad_II:	out integer;
+			datao_II: in unsigned(31 downto 0);
+			
+			ad_II_2: out integer;
+			we_II_2 : out std_logic;
+			datao_II_2: in unsigned(39 downto 0);
+
+         face_detected : out  boolean;
+         send_img : out  boolean
         );
     END COMPONENT;
    --declare inputs and initialize them
@@ -66,7 +127,33 @@ COMPONENT RAM_II_2  --'test' is the name of the module needed to be tested.
 
 
 	
-	
+	signal Image_int_ready :  boolean;
+   --signal clk :  STD_LOGIC;
+   signal rst :  STD_LOGIC;
+					
+	--Communications avec les RAMS
+	signal we_stage : std_logic;
+	signal ad_stage : integer;
+	signal datao_stage : unsigned(27 downto 0);
+					
+	signal we_feature : std_logic;
+	signal ad_feature :  integer;
+	signal datao_feature : unsigned(60 downto 0);
+
+	signal we_rectangle : std_logic;
+	signal ad_rectangle : integer;
+	signal datao_rectangle : unsigned(24 downto 0);
+
+	signal we_II	: std_logic;
+	signal ad_II: integer;
+	signal datao_II: unsigned(31 downto 0);
+			
+	signal ad_II_2: integer;
+	signal we_II_2 : std_logic;
+	signal datao_II_2: unsigned(39 downto 0);
+
+   signal face_detected :  boolean;
+   signal send_img :  boolean;
 	
 			
   BEGIN
@@ -89,6 +176,54 @@ COMPONENT RAM_II_2  --'test' is the name of the module needed to be tested.
 			 data_o=>data_o_2
        );   
 
+
+		uut_3: fsm_detection PORT MAP (
+			 Image_int_ready => Image_int_ready,
+          clk => clk,
+          rst => rst,
+					
+			--Communications avec les RAMS
+			we_stage => we_stage,
+			ad_stage => ad_stage,
+			datao_stage => datao_stage,
+					
+			we_feature => we_feature,
+			ad_feature => ad_feature,
+			datao_feature => datao_feature,
+
+			we_rectangle => we_rectangle,
+			ad_rectangle => ad_rectangle,
+			datao_rectangle => datao_rectangle,
+
+			we_II	=> we_II,
+			ad_II => ad_II,
+			datao_II => datao_II,
+			
+			ad_II_2 => ad_II_2,
+			we_II_2 => we_II_2,
+			datao_II_2 => datao_II_2,
+
+         face_detected => face_detected,
+         send_img => send_img
+       ); 
+		 
+		 uut_4: ram_stages PORT MAP (
+			adress => ad_stage,
+         clk => clk,
+			 data_o=>datao_stage
+       );   
+		 
+		 uut_5: ram_features PORT MAP (
+			adress => ad_feature,
+         clk => clk,
+			 data_o=>datao_feature
+       );   
+		 
+		 uut_6: ram_rectangles PORT MAP (
+			adress => ad_rectangle,
+         clk => clk,
+			 data_o=>datao_rectangle
+       );   
 		
 clk_process :process
    begin
@@ -103,7 +238,6 @@ cpt: process(clk)
 	
 	adress_next<= adress +1;
 	adress_next_2<= adress_2 +1;
-	adress_next_3 <= adress_3 +1;
 
 end process;
 
@@ -169,15 +303,16 @@ if (not endfile(infile)) then   --checking the "END OF FILE" is not reached.
 	adress_2<=adress_next_2;
 
 else
-	we_2 <= '0';
-	termine_2 <= true;
-	if(counter_2 = '0') then
-		adress_2<=(others=>'0');
-		counter_2 <= '1';
-	else 
-		adress_2<=adress_next_2;
-	end if;
-	endoffile_2 <='1';         --set signal to tell end of file read file is reached.
+	Image_int_ready <= true;
+--	we_2 <= '0';
+--	termine_2 <= true;
+--	if(counter_2 = '0') then
+--		adress_2<=(others=>'0');
+--		counter_2 <= '1';
+--	else 
+--		adress_2<=adress_next_2;
+--	end if;
+--	endoffile_2 <='1';         --set signal to tell end of file read file is reached.
 end if;
 wait until clk = '1' and clk'event;
 
