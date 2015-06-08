4a73254e32aa8965969f7bb3d57a99829e4be2db
Saad Ouassil Allak <sw.allak@gmail.com>
2015-06-08 10:43:21 +0200

    Correction alu.vhd

--- ISE/alu.vhd
+++ ISE/alu.vhd
@@ -20,26 +20,26 @@ package body alu is
 	procedure adder (signal in_A : in signed (15 downto 0) ; signal in_B : in signed (15 downto 0); 
 						signal in_param: in std_logic_vector(15 downto 0); signal out_AB : out signed(16 downto 0)
 						; signal out_param : out std_logic_vector (8 downto 0)) is
-variable ae : unsigned;
-variable av : unsigned;
-variable be : unsigned;
-variable bv : unsigned;
-variable re : unsigned (5 downto 0);
-variable rv : unsigned (4 downto 0);
-variable a : integer;
-variable b : integer;
+		variable ae : unsigned (3 downto 0);
+		variable av : unsigned (3 downto 0);
+		variable be : unsigned (3 downto 0);
+		variable bv : unsigned (3 downto 0);
+		variable re : unsigned (4 downto 0);
+		variable rv : unsigned (3 downto 0);
+		variable a : integer;
+		variable b : integer;
 
-begin
-		ae:=unsigned(in_param(15 downto 12));
-		av:=unsigned(in_param(11 downto 8));
-		be:=unsigned(in_param(7 downto 4));
-		bv:=unsigned(in_param(3 downto 0));
-		a:= to_integer(in_A(to_integer(ae+av) downto 0));
-		b:= to_integer (in_B(to_integer(be+bv) downto 0));
+		begin
+			ae:=unsigned(in_param(15 downto 12));
+			av:=unsigned(in_param(11 downto 8));
+			be:=unsigned(in_param(7 downto 4));
+			bv:=unsigned(in_param(3 downto 0));
+			a:= to_integer(in_A(to_integer(ae+av) downto 0));
+			b:= to_integer (in_B(to_integer(be+bv) downto 0));
 		if ae > be then
-			re:=ae;
+			re:= '0' & ae;
 		else
-			re:=be;
+			re:='0' & be;
 		end if;
 
 		if av > bv then
@@ -53,43 +53,44 @@ begin
 		re:=re+1;
 		out_param<= std_logic_vector(re) & std_logic_vector(rv);
 		out_ab<=to_signed((a+b),16);
-
-	end procedure;
+end procedure;
 	
 	
 	procedure mult (signal in_A : in signed (15 downto 0) ; signal in_B : in signed (15 downto 0); 
 					signal in_param: in std_logic_vector(15 downto 0); signal out_AB : out signed(31 downto 0)
 					; signal out_param : out std_logic_vector (9 downto 0))is
-			variable ae : unsigned;
-			variable av : unsigned;
-			variable be : unsigned;
-			variable bv : unsigned;
-			variable re : unsigned (4 downto 0);
-			variable rv : unsigned (4 downto 0);
-			variable a : integer;
-			variable b : integer;
 
-			begin
-				ae:=unsigned(in_param(15 downto 12));
-				av:=unsigned(in_param(11 downto 8));
-				be:=unsigned(in_param(7 downto 4));
-				bv:=unsigned(in_param(3 downto 0));
-				a:= to_integer(in_A(to_integer(ae+av) downto 0));
-				b:= to_integer (in_B(to_integer(be+bv) downto 0));
-				
-				rv:= av+bv;
-				re:= ae+be;
-				
-			out_param<= std_logic_vector(re) & std_logic_vector(rv);
-			out_ab<=to_signed((a*b),32);
-			end procedure;
+		variable ae : unsigned (3 downto 0);
+		variable av : unsigned (3 downto 0);
+		variable be : unsigned (3 downto 0);
+		variable bv : unsigned (3 downto 0);
+		variable re : unsigned (4 downto 0);
+		variable rv : unsigned (4 downto 0);
+		variable a : integer;
+		variable b : integer;
+
+		begin
+			ae:=unsigned(in_param(15 downto 12));
+			av:=unsigned(in_param(11 downto 8));
+			be:=unsigned(in_param(7 downto 4));
+			bv:=unsigned(in_param(3 downto 0));
+			a:= to_integer(in_A(to_integer(ae+av) downto 0));
+			b:= to_integer (in_B(to_integer(be+bv) downto 0));
+			
+			rv:= ('0'& av)+('0'& bv);
+			re:= ('0'& ae)+ ('0'& be);
+			
+		out_param<= std_logic_vector(re) & std_logic_vector(rv);
+		out_ab<=to_signed((a*b),32);
+
+		end procedure;
 			
 function greater (signal in_A : in signed (31 downto 0); signal In_B : in signed (31 downto 0);
-					in_param : in std_logic_vector (19 downto 0)) return boolean	is
-	variable ae : unsigned;
-	variable av : unsigned;
-	variable be : unsigned;
-	variable bv : unsigned;
+				in_param : in std_logic_vector (19 downto 0)) return boolean	is
+	variable ae : unsigned (3 downto 0);
+	variable av : unsigned (3 downto 0);
+	variable be : unsigned (3 downto 0);
+	variable bv : unsigned (3 downto 0);
 	variable a : integer;
 	variable b : integer;
 begin
